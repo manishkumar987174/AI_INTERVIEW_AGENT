d35b76ff61f95702fdbc4de27ce02c8a5d0e8c48
manishkumar987174 <manishkumar987174@gmail.com>
2026-04-10 00:27:37 +0530

    URL issue

--- server/index.js
+++ server/index.js
@@ -11,9 +11,11 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: [
+    "https://ai-interview-agent-1-6819.onrender.com"
+  ],
+  credentials: true
+}));
 
 app.use(express.json())
 app.use(cookieParser())
